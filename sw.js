47712f5b2b8186de33e28843c8451e2b702ea33e
rpi10 <116579105+rpi10@users.noreply.github.com>
2024-09-10 18:14:30 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// Listen for the 'push' event to handle notification
-self.addEventListener('notificationclick', function(event) {
-    event.notification.close();
-    // Logic when the notification is clicked
-    event.waitUntil(
-        clients.openWindow('https://www.example.com') // Replace with the action you'd like
-    );
-});
